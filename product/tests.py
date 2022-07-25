56d40e471b65a6137d9979ca751323ed48d6c418
E-the <ashmitashrestha564@gmail.com>
2022-07-25 15:15:09 +0545

    Test on products app sucessfully

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
 # Create your tests here.
+
+# def TestViews(TestCase):
+#     def setUp(Self):
+        
