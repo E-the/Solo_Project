ea659a839eb982be86ff3a8018ebf0342aa804d1
E-the <ashmitashrestha564@gmail.com>
2022-07-25 15:12:18 +0545

    Test done App app

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.test import SimpleTestCase, TestCase, Client
+from django.urls import reverse, resolve
+from app.views import about, error, gallery, footer, contact
 
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_about_url(self):
+        url = reverse(about)
+        print(url)
+        self.assertEquals(resolve(url).func,about)
+
+    def test_error_url(self):
+        url = reverse(error)
+        print(url)
+        self.assertEquals(resolve(url).func,error)
+
+    def test_gallery_url(self):
+        url = reverse(gallery)
+        print(url)
+        self.assertEquals(resolve(url).func,gallery)
+    
+    def test_footer_url(self):
+        url = reverse(footer)
+        print(url)
+        self.assertEquals(resolve(url).func,footer)
+
+    def test_contact_url(self):
+        url = reverse(contact)
+        print(url)
+        self.assertEquals(resolve(url).func,contact)
